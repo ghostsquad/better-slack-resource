dde40a5039d49ae252e662e5f77f5e97a3bb4075
Wes McNamee <ghost.squadron@gmail.com>
2018-08-02 14:11:18 -0700

    Fix spacing in some model structs

--- resourcemodels/source.go
+++ resourcemodels/source.go
@@ -1,8 +1,8 @@
 package resourcemodels
 
 type Source struct {
-	Url         string 	`json:"url"           validate:"required"`
-	Channel     string 	`json:"channel"`
-	DisablePut  bool 		`json:"disable_put"`
-	Debug       bool 		`json:"debug"`
+	Url        string `json:"url"         validate:"required"`
+	Channel    string `json:"channel"`
+	DisablePut bool   `json:"disable_put"`
+	Debug      bool   `json:"debug"`
 }
